7d63bc1e239aacbfa5ca24feea9c28a5e1c8af6c
Thomas Steenbergen <thomas@ossyn.com>
2025-02-04 22:48:09 +0100

    fix(website): Fixing deployment
    Signed-off-by: Thomas Steenbergen <thomas@ossyn.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ossyn-com', // Usually your GitHub org/user name.
   projectName: 'ossyn-website', // Usually your repo name.
-  trailingSlash: false,
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
